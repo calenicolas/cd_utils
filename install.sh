df904f0085a7aa6ea6ac6d8894e191005a89abce
claudio.ale <claudio.ale@etermax.com>
2023-05-12 17:02:43 -0300

    Deploy service

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 cp -r sbin/* /usr/local/sbin
+mkdir /usr/local/lib/cd_utils || true
+cp -r lib/* /usr/local/lib/cd_utils
+
+cp -r service/cd-utils-deploy.service /lib/systemd/system
+
+systemctl daemon-reload
+systemctl enable cd-utils-deploy
