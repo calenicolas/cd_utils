0d1fd55bee1b969116c71bdbeee9240b50623bce
claudio.ale <claudio.ale@etermax.com>
2023-05-16 13:57:05 -0300

    cd listener

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ mkdir /usr/local/lib/cd_utils || true
 cp -r lib/* /usr/local/lib/cd_utils
 
 cp -r service/systemctl/cd-utils-deploy.service /lib/systemd/system
+cp -r service/systemctl/cd-default-infrastructure.service /lib/systemd/system
+
+docker build -t deploy-service service/docker
 
 systemctl daemon-reload
 systemctl enable cd-utils-deploy
+systemctl enable cd-default-infrastructure
